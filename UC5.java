26942b926fa454eccaf12abab8370a5916f5c6b6
sasanjayadithya <sasanjayadithya@gmail.com>
2026-03-03 20:12:12 +0530

    Add UC5 class to print a banner pattern

---
+++ UC5.java
@@ -0,0 +1,16 @@
+public class UC5 {
+    public static void main(String[] args) {
+        String[] bannerLines = {
+            String.join(" ", " **** ", " **** ", " ****** ", " ******** "),
+            String.join(" ", " *  * ", " *  * ", " *    * ", " *        "),
+            String.join(" ", " *  * ", " *  * ", " *    * ", " *        "),
+            String.join(" ", " *  * ", " *  * ", " ****** ", " ******** "),
+            String.join(" ", " *  * ", " *  * ", " *      ", "        * "),
+            String.join(" ", " *  * ", " *  * ", " *      ", "        * "),
+            String.join(" ", " **** ", " **** ", " *      ", " ******** ")
+        };
+        for (String line : bannerLines) {
+            System.out.println(line);
+        }
+    }
+}
